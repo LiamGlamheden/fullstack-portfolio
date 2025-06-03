d6802bddc8ed6c3d57c8618a97fa8a6ca450b496
Liam <04liagla@skola.boras.se>
2025-06-03 14:50:51 +0200

    add tabs simple styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 export default function Header() {
-    return (
-      <header style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "1rem",
-        backgroundColor: "#334155",
-        color: "white"
-      }}>
-        <img src="/Portfolio-icon.png" alt="Portfolio icon" style={{ height: "4em"}} />
-        <p>My Portfolio</p>
-      </header>
-    );
-  }
-  
+  return (
+    <header style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      padding: "1rem",
+      backgroundColor: "#334155",
+      color: "white"
+    }}>
+        <div style={{ display: "flex", alignItems: "center"}}>
+            <img src="/Portfolio-icon.png" alt="Portfolio icon" style={{ height: "4em" }} />
+            <p>Liam Glamheden</p>
+        </div>
+
+      <nav style={{ display: "flex", gap: "2rem" }}>
+        <p style={{ cursor: "pointer" }}>Resume</p>
+        <p style={{ cursor: "pointer" }}>About</p>
+        <p style={{ cursor: "pointer" }}>Projects</p>
+        <p style={{ cursor: "pointer" }}>Skills</p>
+        <p style={{ cursor: "pointer" }}>Contact</p>
+      </nav>
+    </header>
+  );
+}

57871951f3d95a99396872db441d879216a90e03
Liam <04liagla@skola.boras.se>
2025-06-20 09:27:31 +0200

    add project model and mocked data

---
+++ backend/models/project.cs
@@ -0,0 +1,9 @@
+public class Project
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int GroupSize { get; set; }
+    public string TechStack { get; set; }
+    public DateTime Date { get; set; }
+}
--- backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c7fa50f2cf0d25111df606520c610676539ac8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0060715d7afcf19903480a8f5e9f3e0b8cfcb580")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

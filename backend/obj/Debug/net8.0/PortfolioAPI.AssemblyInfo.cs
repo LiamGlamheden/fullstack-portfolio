4cbc203b2732776933ee909a17e52f969def6377
Liam <04liagla@skola.boras.se>
2025-07-17 21:40:31 +0200

    add fix

--- backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/PortfolioAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e30d832a6406eaf711650452aa45e1fc9ee729")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c63422ac7685b6b6c88606335850c0cf27541d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
